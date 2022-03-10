12c66625bb41dfa0b4a9b2dad0e7c020a655d7bc
SissonJ <sissonjack67@gmail.com>
2022-03-09 23:29:16 -0600

    should be pretty good now

--- shd-scrt-pair.py
+++ shd-scrt-pair.py
@@ -15,10 +15,8 @@ mk = MnemonicKey(mnemonic="easy oxygen bone search trophy soccer video float tin
 client = LCDClient('https://lcd.secret.llc', 'secret-4')
 
 SSWAP_SSCRT_SHD_PAIR = "secret1wwt7nh3zyzessk8c5d98lpfsw79vzpsnerj6d0"
-#SSWAP_SSCRT_SHD_PAIR_HASH = client.wasm.contract_hash(SSWAP_SSCRT_SHD_PAIR)
 SSWAP_QUERY = { 'pool': {} }
 SIENNA_SSCRT_SHD_PAIR = "secret1drm0dwvewjyy0rhrrw485q4f5dnfm6j25zgfe5"
-#SIENNA_SSCRT_SHD_PAIR_HASH = client.wasm.contract_hash(SIENNA_SSCRT_SHD_PAIR)
 SIENNA_QUERY = 'pair_info'
 
 SSCRT_ADRESS = 'secret1k0jntykt7e4g3y88ltc60czgjuqdy4c9e8fzek'
@@ -28,11 +26,7 @@ SHD_ADRESS = 'secret1qfql357amn448duf5gvp9gr48sxx9tsnhupu3d'
 SHD_KEY = 'api_key_xSmcfnyU8z5750bZSC9icFYUz6whMLIUeEloEqORQ7M='
 
 wallet = client.wallet(mk)
-#temp = wallet.account_number_and_sequence()
-#accNum, sequence = temp['account_number'], temp['sequence']
-#testSscrtBal = 10
 fee = StdFee(200001, "050001uscrt")
-#gasPrice = .05975 #secret
 
 def getBalances(scrtBal, sscrtBal, shdBal):
   scrtBalRes = client.bank.balance(mk.acc_address)
@@ -48,32 +42,17 @@ def constantProduct(poolBuy, poolSell, x):
 def getSiennaRatio():
   siennaInfo = client.wasm.contract_query(SIENNA_SSCRT_SHD_PAIR, SIENNA_QUERY)
   shdAmount, sscrtAmount = float(siennaInfo['pair_info']['amount_0']) * 10**-8, float(siennaInfo['pair_info']['amount_1'])*10**-6
-  #print(shdAmount, sscrtAmount)
   return sscrtAmount/shdAmount, shdAmount, sscrtAmount
 
 def getSSwapRatio():
   sswapInfo = client.wasm.contract_query(SSWAP_SSCRT_SHD_PAIR, SSWAP_QUERY)
   sscrtAmount, shdAmount = float(sswapInfo['assets'][0]['amount'])*10**-6, float(sswapInfo['assets'][1]['amount'])*10**-8
-  #print(shdAmount, sscrtAmount)
   return sscrtAmount/shdAmount, shdAmount, sscrtAmount
 
-def calculateProfitability(r1, r2, sscrtBal):
-  #aveprice = (r1 + r2)/2
-  gasFeeScrt = .0425 + .000133
-  #print("gas: ", gasFeeShd ) #.99499999
-  
-  workingBal = sscrtBal * .9 #Change to shdBal when actually trading
-  firstSwap = (.997 * workingBal) / r1
-  #print("firstswap: ", firstSwap)
-  secondSwap = (.997 * firstSwap) * r2
-  #print("secondswap: ", secondSwap)
-  return secondSwap - workingBal - gasFeeScrt, firstSwap, secondSwap
-
-def calculateProfitCP(s1Shd, s1Sscrt, s2Shd, s2Sscrt, amountSwapped):
-  gasFeeScrt = .05 + .00027
-
+def calculateProfitCP(s1Shd, s1Sscrt, s2Shd, s2Sscrt, amountSwapped, gasFeeScrt):
   firstSwap = constantProduct(s1Shd, s1Sscrt, amountSwapped*.996)
   secondSwap = constantProduct(s2Sscrt, s2Shd, firstSwap*.997) * .999
+
   profit = secondSwap - amountSwapped - gasFeeScrt
   return profit, firstSwap, secondSwap 
 
@@ -92,12 +71,15 @@ def swapSienna(sscrtBal, firstSwap, secondSwap):
   handleMsgSswap = { "send": {"recipient": SSWAP_SSCRT_SHD_PAIR, "amount": firstSwapStr, "msg": encryptedMsgSswap }}
   msgExecuteSswap = client.wasm.contract_execute_msg(mk.acc_address, SHD_ADRESS, handleMsgSswap, [])
 
+  #wallet.execute_tx(SHD_ADRESS, handleMsg) #OTHER METHOD
   
   tx = wallet.create_and_sign_tx([msgExecuteSienna, msgExecuteSswap], fee)
-  res = client.tx.broadcast(tx)
+  try:
+    res = client.tx.broadcast(tx)
+  except:
+    print("BROADCAST TX ERROR")
+    return False
   return res
-
-  #wallet.execute_tx(SHD_ADRESS, handleMsg) #OTHER METHOD
 
 def swapSswap(sscrtBal, firstSwap, secondSwap):
   sscrtBalStr = str(int(sscrtBal * 10 ** 6))
@@ -115,28 +97,33 @@ def swapSswap(sscrtBal, firstSwap, secondSwap):
   msgExecuteSienna = client.wasm.contract_execute_msg(mk.acc_address, SHD_ADRESS, handleMsgSienna, [])
 
   #res = wallet.execute_tx(SSCRT_ADRESS, handleMsgSswap) #OTHER METHOD
-  #res = wallet.execute_tx(SHD_ADRESS, handleMsgSienna) #OTHER METHOD
 
   tx = wallet.create_and_sign_tx([msgExecuteSswap, msgExecuteSienna], fee)
-  res = client.tx.broadcast(tx)
-  #tx = asyncio.run(wallet.create_and_sign_tx([msgExecuteSienna]))
-  res = client.tx.broadcast(tx)
+  try:
+    res = client.tx.broadcast(tx)
+  except:
+    print("BROADCAST TX ERROR")
+    return False
   return res
 
-def buyScrt():
-  #Swap 10 sscrt for 10 scrt
-  #NOT IMPLEMENTED
-  return
+def buyScrt(scrtBal):
+  sscrtBalRes = client.wasm.contract_query(SSCRT_ADRESS, { "balance": { "address": mk.acc_address, "key": SSCRT_KEY }})
+  redeemAmount = int(sscrtBalRes['balance']['amount']) - scrtBal * 10 ** 6
+  handleMsg = {"redeem":{"amount":str(redeemAmount)}}
+  res = wallet.execute_tx(SSCRT_ADRESS, handleMsg)
+  return res
 
-def checkScrtBal(scrtBal):
+def checkScrtBal(scrtBal, tradeAmount):
   if (scrtBal < 1 ):
-    buyScrt()
-    return False
+    print("NOT ENOUGH SCRT")
+    buyScrt(tradeAmount)
+    return True
   return True
 
 def checkLastTradeProfitable(sscrtBal, lastSscrtBal):
   if( not (sscrtBal == lastSscrtBal) and (sscrtBal - lastSscrtBal < .0425 + .00027) ):
-    return True #SET TO TRUE BECAUSE CALCULATION CHANGED
+    print("NOT ENOUGH PROFIT")
+    return False 
   return True
 
 def txHandle(txResponse, profit, logWriter, runningProfit):
@@ -144,7 +131,7 @@ def txHandle(txResponse, profit, logWriter, runningProfit):
   print(txResponse.to_json())
   if(txResponse.is_tx_error()):
     print( "ERROR" )
-    logWriter.writerow([datetime.now(), "Error"])
+    logWriter.writerow([datetime.now(), "Error", txResponse.txhash])
     print(txResponse.txhash)
     return False
 
@@ -167,7 +154,7 @@ def sync_next_block(secret, height=0):
         
 
 def main():
-  txLog = open("tx_log.csv", "a")
+  txLog = open("shd-scrt-pair-log.csv", "a")
   logWriter = csv.writer(txLog, delimiter=' ')
   scrtBal, sscrtBal, shdBal, lastSscrtBal = 0, 0, 0, 0
   keepLooping = True
@@ -175,40 +162,49 @@ def main():
   runningProfit = 0
   amountSwapping = 40 #sscrt
   lastHeight = 0
+  gasFeeScrt = .050001 + .00027
   while keepLooping:
     try:
       lastHeight = sync_next_block(client, lastHeight)
-      print( "Height: ", lastHeight )
-      print(datetime.now())
+      #print( "Height: ", lastHeight )
+      #print(datetime.now())
       txResponse = ""
       lastSscrtBal = sscrtBal
       #scrtBal, sscrtBal, shdBal = getBalances(scrtBal, sscrtBal, shdBal)
       scrtBal = int(client.bank.balance(mk.acc_address).to_data()[0]["amount"]) * 10**-6
-      keepLooping = checkScrtBal(scrtBal)# and checkLastTradeProfitable(sscrtBal, lastSscrtBal)
+      keepLooping = checkScrtBal(scrtBal, amountSwapping) and checkLastTradeProfitable(sscrtBal, lastSscrtBal)
       siennaRatio, siennaShd, siennaSscrt = getSiennaRatio()
       sswapRatio, sswapShd, sswapSscrt = getSSwapRatio()
+    except:
+      print("ERROR in queries\n")
+      continue
+    try:
       difference = siennaRatio - sswapRatio 
-      print("Sienna: ", siennaRatio)
-      print(" SSwap: ", sswapRatio)
-      print("  Diff: ", difference)
+      #print("Sienna: ", siennaRatio)
+      #print(" SSwap: ", sswapRatio)
+      #print("  Diff: ", difference)
       profit= firstSwap = secondSwap = 0
       if( difference > 0 ):
-        profit, firstSwap, secondSwap = calculateProfitCP(sswapShd, sswapSscrt, siennaShd, siennaSscrt, amountSwapping)
-        print("profit: ", profit)
+        profit, firstSwap, secondSwap = calculateProfitCP(sswapShd, sswapSscrt, siennaShd, siennaSscrt, amountSwapping, gasFeeScrt)
+        #print("profit: ", profit)
       if( difference < 0 ):
-        profit, firstSwap, secondSwap = calculateProfitCP(siennaShd, siennaSscrt, sswapShd, sswapSscrt, amountSwapping)
-        print("profit: ", profit)
+        profit, firstSwap, secondSwap = calculateProfitCP(siennaShd, siennaSscrt, sswapShd, sswapSscrt, amountSwapping, gasFeeScrt)
+        #print("profit: ", profit)
       if( profit > 0 and difference > 0):
+        print( "Height: ", lastHeight )
+        print(datetime.now())
+        print("profit: ", profit)
         txResponse = swapSswap(amountSwapping, firstSwap, secondSwap)
       if( profit > 0 and difference < 0):
+        print( "Height: ", lastHeight )
+        print(datetime.now())
+        print("profit: ", profit)
         txResponse = swapSienna(amountSwapping, firstSwap, secondSwap)
       if( txResponse != ""):
         runningProfit += profit
         txHandle(txResponse, profit, logWriter, runningProfit)
-      
-      print()
     except:
-      print( "ERROR\n" )
+      print( "ERROR in tx\n" )
       continue
   print( runningProfit )
   txLog.close()
@@ -221,16 +217,17 @@ def testFunction():
   #logWriter.writerow([datetime.now(), "Hello2", "this2"])
   #txLog.close()
   amountSwapping = 40
-  siennaRatio, siennaShd, siennaSscrt = getSiennaRatio()
-  sswapRatio, sswapShd, sswapSscrt = getSSwapRatio()
-  difference = siennaRatio - sswapRatio 
-  if( difference > 0 ):
-    profit, firstSwap, secondSwap = calculateProfitCP(sswapShd, sswapSscrt, siennaShd, siennaSscrt, amountSwapping)
-    print("profit: ", profit, firstSwap, secondSwap)
-  if( difference < 0 ):
-    profit, firstSwap, secondSwap = calculateProfitCP(siennaShd, siennaSscrt, sswapShd, sswapSscrt, amountSwapping)
-    print("profit: ", profit, firstSwap, secondSwap)
-  res = swapSswap(40, firstSwap, secondSwap)
+  #siennaRatio, siennaShd, siennaSscrt = getSiennaRatio()
+  #sswapRatio, sswapShd, sswapSscrt = getSSwapRatio()
+  #difference = siennaRatio - sswapRatio 
+  #if( difference > 0 ):
+  #  profit, firstSwap, secondSwap = calculateProfitCP(sswapShd, sswapSscrt, siennaShd, siennaSscrt, amountSwapping)
+  #  print("profit: ", profit, firstSwap, secondSwap)
+  #if( difference < 0 ):
+  #  profit, firstSwap, secondSwap = calculateProfitCP(siennaShd, siennaSscrt, sswapShd, sswapSscrt, amountSwapping)
+  #  print("profit: ", profit, firstSwap, secondSwap)
+  #res = swapSswap(40, firstSwap, secondSwap)
+  res = buyScrt(amountSwapping)
   print(res.to_json())
 
 #testFunction()
