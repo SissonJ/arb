f1e9b26e67dd8915def0cb414cc130b0bb858325
SissonJ <sissonjack67@gmail.com>
2022-11-14 04:58:07 +0000

    tweaks

--- src/config.py
+++ src/config.py
@@ -56,8 +56,8 @@ config = {
     "centralLogLoc": central_logs["arb_v3"]["log"],
     "outputLogLoc": "./../logs/sscrt-shd.log",
     "fee": {
-      "gas": 3200000,
-      "price": "40000uscrt",
+      "gas": 4000000,
+      "price": "100000uscrt",
     },
   },
   "sscrt-seth-config": {
@@ -94,8 +94,8 @@ config = {
     "centralLogLoc": central_logs["arb_v3"]["log"],
     "outputLogLoc": "./../logs/sscrt-seth.log",
     "fee": {
-      "gas": 3200000,
-      "price": "40000uscrt",
+      "gas": 4000000,
+      "price": "100000uscrt",
     },
   },
   "sscrt-swbtc-config": {
@@ -132,8 +132,8 @@ config = {
     "centralLogLoc": central_logs["arb_v3"]["log"],
     "outputLogLoc": "./../logs/sscrt-swbtc.log",
     "fee": {
-      "gas": 3200000,
-      "price": "40000uscrt",
+      "gas": 4000000,
+      "price": "100000uscrt",
     },
   },
   "sscrt-susdt-config": {
@@ -170,8 +170,8 @@ config = {
     "centralLogLoc": central_logs["arb_v3"]["log"],
     "outputLogLoc": "./../logs/sscrt-susdt.log",
     "fee": {
-      "gas": 3200000,
-      "price": "40000uscrt",
+      "gas": 4000000,
+      "price": "100000uscrt",
     },
   },
   "sscrt-sxmr-config": {
@@ -208,8 +208,8 @@ config = {
     "centralLogLoc": central_logs["arb_v3"]["log"],
     "outputLogLoc": "./../logs/sscrt-seth.log",
     "fee": {
-      "gas": 3200000,
-      "price": "40000uscrt",
+      "gas": 4000000,
+      "price": "100000uscrt",
     },
   },
   "sscrt-sienna-config": {
@@ -246,8 +246,8 @@ config = {
     "centralLogLoc": central_logs["arb_v3"]["log"],
     "outputLogLoc": "./../logs/sscrt-sienna.log",
     "fee": {
-      "gas": 3200000,
-      "price": "40000uscrt",
+      "gas": 4000000,
+      "price": "100000uscrt",
     },
   },
 }
